3a1f629656dfba0376f444af1ec1a0cdb429cbb3
jerson1207 <jersonjabatstudent@gmail.com>
2022-01-13 03:08:08 +0800

    add login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   root 'static_page#home'
   resources :menus
   get '/signup', to: 'users#new'
-  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
