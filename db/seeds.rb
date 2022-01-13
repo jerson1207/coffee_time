4d9a2cfcf91484313d7473005b34c592e4ee2bac
jerson1207 <jersonjabatstudent@gmail.com>
2022-01-13 16:45:51 +0800

    add user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
 admin = User.new(name: "Admin", email:"admin@test.com", role: 1, password: "123456", password_confirmation: "123456")
 admin.save
+puts "admin has created"
+user = User.new(name: "User Juan", email:"user1@test.com", role: 0, password: "123456", password_confirmation: "123456")
+user.save
