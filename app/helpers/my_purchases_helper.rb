a34020c06dfc394bd8f8ac3b90255674ffa54663
jerson1207 <jersonjabatstudent@gmail.com>
2022-01-19 06:51:01 +0800

    add nothing to display

--- app/helpers/my_purchases_helper.rb
+++ app/helpers/my_purchases_helper.rb
@@ -1,2 +1,37 @@
 module MyPurchasesHelper
+
+  def nothing_to_display_cart
+    cart = Order.where(status: "in_cart", user_id: current_user.id ).count
+    if cart == 0
+      return "Nothing to display"
+    end
+  end
+
+  def nothing_to_display_cart
+    cart = Order.where(status: "in_cart", user_id: current_user.id ).count
+    if cart == 0
+      return "Nothing to display"
+    end
+  end
+
+  def nothing_to_display_preparing
+    preparing = Order.where(status: "preparing", user_id: current_user.id ).count
+    if preparing == 0
+      return "Nothing to display"
+    end
+  end
+
+  def nothing_to_display_shipping
+    shipping = Order.where(status: "shipping", user_id: current_user.id ).count
+    if shipping == 0
+      return "Nothing to display"
+    end
+  end
+
+  def nothing_to_display_complete
+    complete = Order.where(status: "complete", user_id: current_user.id ).count
+    if complete == 0
+      return "Nothing to display"
+    end
+  end
 end
