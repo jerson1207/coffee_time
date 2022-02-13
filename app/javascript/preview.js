98d4b453a8498ef4cf7743891529c05e67d9f902
jerson1207 <jersonjabatstudent@gmail.com>
2022-02-13 14:44:39 +0800

    add reload page once at menu and change color of green text to yellow text

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,8 +1,27 @@
-let preveiwContainer = document.querySelector('.products-preview');
-let previewBox = preveiwContainer.querySelectorAll('.preview');
+// const preveiwContainer = document.querySelector('.products-preview');
+// const previewBox = preveiwContainer.querySelectorAll('.preview');
 
+// document.querySelectorAll('.products-container .product').forEach(product =>{
+//   product.onclick = () => {
+    
+//     preveiwContainer.style.display = 'flex';
+//     let name = product.getAttribute('data-name');
+//     previewBox.forEach(preview =>{
+//       let target = preview.getAttribute('data-target');
+//       if(name == target){
+//         preview.classList.add('active');
+//       }
+//     });
+    
+//   };
+// });
+
+
+const preveiwContainer = document.querySelector('.products-preview');
+const previewBox = preveiwContainer.querySelectorAll('.preview');
 document.querySelectorAll('.products-container .product').forEach(product =>{
-  product.onclick = () => {
+  product.addEventListener('click', function () { 
+    
     
     preveiwContainer.style.display = 'flex';
     let name = product.getAttribute('data-name');
@@ -10,11 +29,12 @@ document.querySelectorAll('.products-container .product').forEach(product =>{
       let target = preview.getAttribute('data-target');
       if(name == target){
         preview.classList.add('active');
+
       }
-    });
-    
-  };
+    });  
+  });
 });
+
 
 previewBox.forEach(close =>{
   close.querySelector('.fa-times').onclick = () =>{
